f1b16d2b9d88a7291b71e784d726db1903fed2ea
Ed Carney <carneyedwardj@gmail.com>
2025-03-15 18:38:23 -0400

    Updates naming of sixel constants

---
+++ TermSixels/Constants/SixelControlStatements.cs
@@ -0,0 +1,12 @@
+namespace TermSixels.Constants;
+
+internal class SixelControlStatements
+{
+    internal const string StartSixelSeq = "\x1bPq";
+
+    internal const string TerminateSixelSeq = "\x1b";
+
+    internal const char CarriageReturn = '$';
+
+    internal const char NewLine = '-';
+}
