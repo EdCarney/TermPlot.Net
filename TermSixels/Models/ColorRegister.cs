9fbe39fa6b5f53b3f9d7179a935981a68ab81a5b
Ed Carney <carneyedwardj@gmail.com>
2025-03-16 23:58:45 -0400

    Adds models for sixel map and color register

---
+++ TermSixels/Models/ColorRegister.cs
@@ -0,0 +1,47 @@
+using System.Drawing;
+
+namespace TermSixels.Models;
+
+public struct ColorRegister
+{
+    private static int _totalRegisterCount = 0;
+
+    public Color Color { get; private set; }
+
+    public int Red { get; private set; }
+    public int Green { get; private set; }
+    public int Blue { get; private set; }
+    public (int, int, int) RgbValues => (Red, Green, Blue);
+
+    public int Hue { get; private set; }
+    public int Lightness { get; private set; }
+    public int Saturation { get; private set; }
+    public (int, int, int) HlsValues => (Hue, Lightness, Saturation);
+
+    public int RegisterNumber { get; }
+
+    public ColorRegister(Color color)
+    {
+        SetColor(color);
+        RegisterNumber = _totalRegisterCount++;
+    }
+
+    private void SetColor(Color color)
+    {
+        Color = color;
+
+        Red = 100 * Color.R / Byte.MaxValue;
+        Green = 100 * Color.G / Byte.MaxValue;
+        Blue = 100 * Color.B / Byte.MaxValue;
+
+        Hue = (int)Color.GetHue();
+        Lightness = (int)(100 * Color.GetBrightness());
+        Saturation = (int)(100 * Color.GetSaturation());
+    }
+
+    public string ToSixelSetString()
+        => $"#{RegisterNumber};1;{Red};{Green};{Blue}";
+
+    public string ToSixelUseString()
+        => $"#{RegisterNumber}";
+}
---
+++ TermSixels/Models/SixelMap.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using TermSixels.Constants;
+
+namespace TermSixels.Models;
+
+public class SixelMap
+{
+    // sixels must be printed row by row, with all colors on a row printed
+    // in sequence before moving on to the next row; so sixel dimensions
+    // are row -> color -> col
+    private readonly Sixel[][][] _sixels;
+
+    public ColorRegister[] ColorRegisters { get; }
+
+    public int Height { get; }
+
+    public int Width { get; }
+
+    public SixelMap(PixelMap pixelMap)
+    {
+        Height = pixelMap.Height / 6;
+        Width = pixelMap.Width;
+
+        ColorRegisters = pixelMap
+            .AllUniqueColors
+            .Select(color => new ColorRegister(color))
+            .ToArray();
+
+        _sixels = new Sixel[Height]
+            .Select(row => new Sixel[ColorRegisters.Count()]
+                .Select(col => new Sixel[Width])
+                .ToArray())
+            .ToArray();
+
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < ColorRegisters.Count(); j++)
+            {
+                for (int k = 0; k < Width; k++)
+                {
+                    int rangeStart = i * 6;
+                    int rangeEnd = (i + 1) * 6;
+                    var colorMap = pixelMap[rangeStart..rangeEnd, k]
+                        .Select(color => color == ColorRegisters[j].Color)
+                        .ToArray();
+
+                    _sixels[i][j][k] = new Sixel(new(colorMap));
+                }
+            }
+        }
+
+    }
+
+    public string ToSixelSequenceString()
+    {
+        var sb = new StringBuilder(SixelControlStatements.StartSixelSeq);
+
+        sb.AppendLine();
+        foreach (var colorReg in ColorRegisters)
+            sb.Append(colorReg.ToSixelSetString());
+
+        sb.AppendLine();
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < ColorRegisters.Count(); j++)
+            {
+                sb.Append(ColorRegisters[j].ToSixelUseString());
+                for (int k = 0; k < Width; k++)
+                {
+                    sb.Append(_sixels[i][j][k].SixelChar);
+                }
+                sb.Append(SixelControlStatements.CarriageReturn);
+            }
+            sb.Append(SixelControlStatements.NewLine);
+            sb.AppendLine();
+        }
+
+        sb.AppendLine(SixelControlStatements.TerminateSixelSeq);
+
+        return sb.ToString();
+    }
+}
